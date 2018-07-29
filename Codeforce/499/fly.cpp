dd47c739f7344a24cce6dc1b01d8b9fe4ed047e0
Udesh Kumarasinghe <udesh.upalanka@live.com>
2018-07-29 11:48:15 +0530

    499 Answers

---
+++ Codeforce/499/fly.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n, a[n], b[n];
+    double m, fuel = 0;
+    cin >> n;
+    cin >> m;
+    for(size_t i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] <= 1) {
+            cout << -1;
+            return 0;
+        }   
+    }
+    for(size_t i = 0; i < n; i++)
+    {
+        cin >> b[i];
+        if (b[i] <= 1) {
+            cout << -1;
+            return 0;
+        }
+    }
+    fuel += m / (b[0] - 1); 
+    // cout << fuel << " ";
+    for(size_t i = n - 1; i > 0; i--)
+    {
+        fuel += (m + fuel) / (a[i] - 1); 
+        // cout << fuel << " ";
+        fuel += (m + fuel) / (b[i] - 1); 
+        // cout << fuel << " ";
+    }
+    fuel += (m + fuel) / (a[0] - 1); 
+    cout << fuel << " ";
+    return 0;
+}
