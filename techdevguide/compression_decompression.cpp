27bdfd4988d1fc1eba1fbed934169e00681fe3cf
Udesh Kumarasinghe <mail@udeshuk.me>
2019-02-10 23:51:48 +0530

    Removed redundent files

---
+++ techdevguide/compression_decompression.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string decompose(string in) {
+    char first = in[0];
+    if (isdigit(first)) {
+        return 
+    } else if (isalpha(first)) {
+
+    } else {
+
+    }
+    return in;
+}
+
+int main() {
+    string in;
+    // cin >> in;
+    in = "3[abc]4[ab]c";
+    cout << decompose(in) << endl;
+}
